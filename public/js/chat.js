78c4bfe9d4ecd89a8012b90fd21ef7bfb60fe0a3
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-21 21:17:55 -0600

    Add location sending

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,5 +8,24 @@ document.querySelector('#msg-form').addEventListener('submit', (e) => {
   e.preventDefault()
 
   const message = e.target.elements.message.value
-  socket.emit('sendMessage', message)
+  socket.emit('sendMessage', message, (error) => {
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message delivered!')
+  })
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser')
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const message = `Location: ${position.coords.latitude}, ${position.coords.longitude}`
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  })
 })
